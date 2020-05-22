8d0fd80d72411ccb71fef33d2fdb2096d3f41602
Gal Dayagi <galdil@gmail.com>
2020-05-22 14:07:20 +0300

    add server and html

---
+++ instagram-style/server.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const path = require("path");
+const app = express();
+
+app.get("/", function (req, res) {
+  res.status(200).sendFile(path.join(__dirname + "/index.html"));
+});
+
+const port = process.env.port || 8080;
+
+app.listen(port, () => {
+  console.log("App running at port: " + port);
+});
