056b9f40a420cdcb2192f790b8747cf57f1ad5bc
Gal Dayagi <galdil@gmail.com>
2020-05-22 15:54:10 +0300

    update aws

--- instagram-style/server.js
+++ instagram-style/server.js
@@ -1,13 +1,23 @@
 const express = require("express");
+const AWS = require("aws-sdk");
 const path = require("path");
 const app = express();
+const config = require("./config.js");
 
-app.get("/", function (req, res) {
-  res.status(200).sendFile(path.join(__dirname + "/client/index.html"));
+app.use(express.static(path.join(__dirname, "client")));
+
+app.get("/", (req, res) => {
+  res.status(200).sendFile("index.html");
 });
 
 const port = process.env.port || 8080;
 
 app.listen(port, () => {
   console.log("App running at port: " + port);
+});
+
+const awsConfig = new AWS.Config({
+  accessKeyId: config.accessKeyId,
+  secretAccessKey: config.secretAccessKey,
+  region: config.region,
 });
